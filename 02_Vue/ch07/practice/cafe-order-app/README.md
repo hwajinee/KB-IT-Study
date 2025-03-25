f236044db9aa5ade576233eed3b94d0469a64152
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-03-25 17:57:45 +0900

    practice start

---
+++ 02_Vue/ch07/practice/cafe-order-app/README.md
@@ -0,0 +1,8 @@
+# Vue app 템플릿
+
+### 템플릿 사용법
+
+1. 해당 프로젝트를 복사한 뒤, 원하는 프로젝트명으로 폴더명 수정
+2. npm install (npm i)
+   package-json 파일을 기반으로 환경 설정
+3. npm run dev로 서버 구동
---
+++ 02_Vue/ch07/practice/cafe-order-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>카페 주문 앱</h1>
+  <h3>메뉴 선택</h3>
+  <div class="menu-box">메뉴</div>
+  <MenuList />
+</template>
+
+<script>
+import MenuList from './components/MenuList.vue';
+
+export default {
+  name: 'App',
+  components: { MenuList },
+  data() {
+    return {
+      menuList: [
+        { name: '아메리카노', price: 4000 },
+        { name: '카페라떼', price: 4500 },
+        { name: '바닐라라떼', price: 4800 },
+        { name: '콜드브루', price: 5000 },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
