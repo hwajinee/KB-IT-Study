30fddd2f4df259e87ba20288743248876503f514
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-03-25 16:48:56 +0900

    vue project

---
+++ 02_Vue/ch07/0.test-vue-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr />
+    <!-- CheckboxItem을 쓰려면 script에 등록해줘야 함. -->
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+  <main></main>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+  // 지역 컴포넌트 등록 방법, 해당 vue에서만 사용 가능
+  components: { CheckboxItem },
+};
+</script>
+
+<style scoped>
+/* style태그에 scoped를 쓰면, 해당 파일에만 style을 적용할 것이라는 뜻 */
+</style>
---
+++ 02_Vue/ch07/0.test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li><input type="checkbox" v-model="checked" />옵션1</li>
+</template>
+
+<script>
+export default {
+  name: 'checkboxItem',
+  data() {
+    return {
+      checked: false,
+    };
+  },
+};
+</script>
