b831fc22c0eb59edfa46e312e25d0cf1897efc34
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-03-26 15:46:07 +0900

    slot-test

---
+++ 02_Vue/ch08/slot-test/src/components/CheckBox2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-changed', { id, checked: $event.target.checked })"
+    />
+
+    <!-- <span
+      v-if="checked === true"
+      style="color: blue; text-decoration: underline"
+    >
+      <i>{{ label }}</i></span
+    >
+    <span v-else style="color: gray">{{ label }}</span> -->
+    <slot>Item</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckBox2',
+  props: ['id', 'checked'],
+};
+</script>
+
+<style scoped></style>
---
+++ 02_Vue/ch08/slot-test/src/main.js
@@ -0,0 +1,6 @@
+// 전역 CSS
+// import './assets/main.css'
+
+import { createApp } from 'vue';
+import App from './App.vue';
+createApp(App).mount('#app');
