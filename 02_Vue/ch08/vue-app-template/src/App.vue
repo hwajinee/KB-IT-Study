af832fdc0ef2ec3a6552bc137ba61a0651832ee9
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-03-26 15:44:15 +0900

    [Vue] ch08 vue-app-template created

---
+++ 02_Vue/ch08/vue-app-template/src/App.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+import ExComponent from './components/ExComponent.vue';
+
+export default {
+  name: 'App',
+  components: { ExComponent },
+};
+</script>
+
+<style scoped></style>
