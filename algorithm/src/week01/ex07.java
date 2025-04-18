b83d86ddbb8a77b165b9e1c095d5a9d33101c3db
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-04-18 17:42:02 +0900

    ì전화번호 목록

--- algorithm/src/week01/ex07.java
+++ algorithm/src/week01/ex07.java
@@ -1,4 +1,22 @@
 package week01;
 
+import java.util.Arrays;
+
 public class ex07 {
+    public static void main(String[] args) {
+
+         String[] phone_book = {"119", "97674223", "1195524421"};
+
+         // 전화번호 정렬
+         Arrays.sort(phone_book);
+
+         boolean result = true;
+
+         for(int i=0; i<phone_book.length; i++){
+             for(int j=i+1; j< phone_book.length; j++){
+                 if(phone_book[j].startsWith(phone_book[i])) result = false;
+             }
+         }
+         System.out.println(result);
+    }
 }
--- algorithm/src/week01/ex07_2.java
+++ algorithm/src/week01/ex07_2.java
@@ -1,4 +1,24 @@
 package week01;
 
+import java.util.HashMap;
+
 public class ex07_2 {
+    public static void main(String[] args) {
+
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String s : phone_book){
+            map.put(s,0);
+        }
+
+        for(String s : map.keySet()){
+            for(int i=0; i<s.length(); i++){
+                if(map.containsKey(s.substring(0,i))) System.out.println("false");
+            }
+        }
+
+
+    }
 }
