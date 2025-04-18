bdbb8eb23e4f06441560b5598a6c37c7c3f473cd
hvvxzin99@gmail.com <hvvxzin99@gmail.com>
2025-04-18 17:41:08 +0900

    완주하지 못한 선수

--- algorithm/src/week01/ex05.java
+++ algorithm/src/week01/ex05.java
@@ -1,4 +1,33 @@
 package week01;
 
+import java.util.HashMap;
+
 public class ex05 {
+    public static void main(String[] args) {
+        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = {"josipa",  "filipa", "marina", "nikola"};
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String s : completion) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(String s : participant) {
+            if(!map.containsKey(s)) {
+                result.append(s);
+            } else {
+                if( map.get(s) > 1) {
+                    map.replace(s, map.get(s) - 1);
+                } else if (map.get(s) == 0) {
+                    result.append(s);
+                }
+            }
+        }
+        System.out.println(result);
+
+
+
+    }
 }
--- algorithm/src/week01/ex05_2.java
+++ algorithm/src/week01/ex05_2.java
@@ -2,31 +2,27 @@ package week01;
 
 import java.util.HashMap;
 
-public class ex05 {
+//미완 !!!!
+public class ex05_2 {
     public static void main(String[] args) {
         String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
         String[] completion = {"josipa",  "filipa", "marina", "nikola"};
 
         HashMap<String, Integer> map = new HashMap<>();
 
-        for(String s : completion) {
+        for(String s : participant) {
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
-        StringBuilder result = new StringBuilder();
-        for(String s : participant) {
-            if(!map.containsKey(s)) {
-                result.append(s);
-            } else {
-                if( map.get(s) > 1) {
-                    map.replace(s, map.get(s) - 1);
-                } else if (map.get(s) == 0) {
-                    result.append(s);
-                }
+        for(String s : completion) {
+            map.replace(s, map.replace(s, map.get(s) - 1));
+        }
+
+        for(String s : map.keySet()){
+            if( map.get(s) >= 1 ){
+                System.out.println(s);
             }
         }
-        System.out.println(result);
-
 
 
     }
